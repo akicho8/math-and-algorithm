afc0b448a125cbc80f5562aac690be93ad96d930
akicho8 <akicho8@gmail.com>
2022-04-03 23:22:10 +0900

    [feat] atcoder-cli 導入
    cargo-atcoder → cargo-compete (RustはこれでOK) → (Ruby編) atcoder_tools → atcoder_greedy → green_day → oj (汎用) → atcoder-cli (いまここ)

--- ruby/math-and-algorithm/spec/091_spec.rb
+++
@@ -1,3 +0,0 @@
-RSpec.describe 'test' do
-
-end
--- ruby/setup.sh
+++ ruby/setup.sh
@@ -3,4 +3,42 @@
 # green_day login
 # green_day new math-and-algorithm
 # green_day new abc150
-green_day2 new math-and-algorithm
+# green_day2 new math-and-algorithm
+
+# pip3 install online-judge-tools
+# yarn global add atcoder-cli
+# acc check-oj
+# acc login
+# acc session
+# /Users/ikeda/Library/Application Support/online-judge-tools/cookie.jar
+
+# acc config
+# acc config default-task-choice inquire
+# acc config default-test-dirname-format test
+
+# acc config-dir
+# acc config
+# ~/Library/Preferences/atcoder-cli-nodejs/config.json
+
+# rm -fr ./abc100
+# acc contest abc100
+# acc tasks abc100
+# acc new -f abc100
+
+# cd abc100/a
+# oj t -c "ruby main.rb" -d tests
+
+# acc contest math-and-algorithm
+# acc tasks math-and-algorithm
+
+# rm -fr math-and-algorithm
+# acc new math-and-algorithm
+
+cd math-and-algorithm/001
+echo "p gets.to_i + 5" > main.rb
+oj t -c "ruby main.rb"
+# oj t -c "ruby main.rb" -d tests
+# acc s main.rb
+#
+# oj submit -y https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_a main.rb
+# acc s main.rb -- -y
