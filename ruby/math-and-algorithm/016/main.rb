1661f039966b96838fdd69d889dbf1202bf35763
akicho8 <akicho8@gmail.com>
2022-04-08 08:31:10 +0900

    [feat] 16

--- ruby/math-and-algorithm/016/main.rb
+++ ruby/math-and-algorithm/016/main.rb
@@ -1,13 +1,23 @@
-# $stdin = DATA
-# a = gets.to_i
-# b, c = gets.split.map(&:to_i)
-# s = gets.strip
-# puts "#{a+b+c} #{s}"
-# __END__
-# 1
-# 2 3
-# 6
-# 
+# [016] Greatest Common Divisor of N Integers
+# https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_p
+gcd = -> a, b {
+  loop do
+    if a.zero?
+      break b
+    end
+    if b.zero?
+      break a
+    end
+    if a < b
+      b = b.modulo(a)
+    else
+      a = a.modulo(b)
+    end
+  end
+}
+# gcd.(11, 88)                    # => 11
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i).sum
-# >> 0
+av = gets.split.take(n).collect(&:to_i)
+# av = [11, 88, 22]
+p av.inject {|a, e| gcd.(a, e) }
+# >> 11
